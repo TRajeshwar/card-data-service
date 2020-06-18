92547ae6dccb77793967a26baa006e18f303ff67
akankshasuryavanshi <akankshasuryavanshi89@gmail.com>
2020-06-19 00:39:29 +0100

    Initial version

---
+++ card-data-service/app.js
@@ -0,0 +1,40 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+
+
+var getCardDetailsRouter = require('./routes/getCardDetails');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/mongo');
+app.use('/getcarddetails', getCardDetailsRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
